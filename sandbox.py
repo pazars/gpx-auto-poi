f348a308f2278e0d84017b0b184d17f653baa14c
Davis Pazars <davis.pazars@protonmail.com>
2023-07-08 20:34:28 +0300

    Basic working version

---
+++ sandbox.py
@@ -0,0 +1,211 @@
+import numpy as np
+import panel as pn
+
+import gpxpy
+import overpass
+import geopy.distance
+import pprint
+import folium
+
+from pathlib import Path
+
+GPX_PATH = Path("C:/Users/davis/OneDrive/Documents/gpx-auto-poi/test.gpx")
+MIN_N_ROUTE_SPLITS = 10
+DISTANCE_THRESHOLD_KM = 5
+
+
+def get_num_splits(n):
+    if n <= 0:
+        return []
+    divisors = [1, n]
+    for div in range(1, int(n**0.5 + 1)):
+        if n % div == 0:
+            divisors.extend([n // div, div])
+    unique_divs = np.array(list(set(divisors)))
+    return int(np.where(unique_divs >= MIN_N_ROUTE_SPLITS, unique_divs, np.inf).min())
+
+
+def find_closest_to_point(response, lat, lon):
+    dists = []
+    qlocs = []
+
+    for feature in response["features"]:
+        qlon, qlat = feature["geometry"]["coordinates"]
+        # Distance between (#lat, lon)
+        lon, lat = lons[len(lons) // 2], lats[len(lats) // 2]
+        dists += [geopy.distance.distance((lat, lon), (qlat, qlon))]
+        qlocs += [(qlat, qlon)]
+
+    dists = np.array(dists)
+
+    min_dist = np.min(dists)
+    min_dist_idx = np.argmin(dists)
+    min_lat, min_lon = qlocs[min_dist_idx]
+
+    return (min_lat, min_lon, min_dist)
+
+
+def closest_in_split(flat, flon, split_lats, split_lons):
+    min_dist = DISTANCE_THRESHOLD_KM
+    min_lat, min_lon = None, None
+    # Check every third coordinate to make faster
+    for n in range(0, len(split_lats), 3):
+        lat, lon = split_lats[n], split_lons[n]
+        dist = geopy.distance.distance((lat, lon), (flat, flon))
+        if dist.kilometers < min_dist:
+            min_dist = dist.kilometers
+            min_lat, min_lon = lat, lon
+
+    if min_lat:
+        return (min_lat, min_lon, min_dist)
+    return None
+
+
+def _get_poi_info(gpx):
+    # Split route into sections and get gpx bounding box coordinates
+
+    lons = []
+    lats = []
+
+    for point in gpx.tracks[0].segments[0].points:
+        lons += [point.longitude]
+        lats += [point.latitude]
+
+    lats = np.array(lats)
+    lons = np.array(lons)
+
+    num_coords = len(lons)
+    num_splits = get_num_splits(len(lons))
+
+    split_lons = np.reshape(lons, (num_splits, -1))
+    split_lats = np.reshape(lats, (num_splits, -1))
+
+    bbox = f"{np.min(lats), np.min(lons), np.max(lats), np.max(lons)}"
+
+    # Perform query
+
+    api = overpass.API()
+
+    query = f"""node["amenity"="drinking_water"]{bbox}"""
+
+    response = api.get(query)
+
+    # Find closest split
+    pois = []
+    for feature in response["features"]:
+        qlon, qlat = feature["geometry"]["coordinates"]
+        closest_dist = geopy.distance.Distance(10)  # km
+        feature_split_idx = 0
+
+        for idx in range(num_splits):
+            flon = split_lons[idx][num_coords // num_splits // 2]
+            flat = split_lats[idx][num_coords // num_splits // 2]
+            dist = geopy.distance.distance((flat, flon), (qlat, qlon))
+            if dist < closest_dist:
+                closest_dist = dist
+                feature_split_idx = idx
+
+        if closest_dist.kilometers > DISTANCE_THRESHOLD_KM:
+            continue
+
+        min_info = closest_in_split(
+            qlat,
+            qlon,
+            split_lats[feature_split_idx],
+            split_lons[feature_split_idx],
+        )
+
+        if not min_info:
+            continue
+
+        poi_info = {
+            "feature_coords": (qlat, qlon),
+            # "Closest route lat, lon": min_info[:2],
+            "distance_km": min_info[2],
+        }
+
+        pois.append(poi_info)
+
+    return pois
+
+
+def _display_start_finish(route_map, gpx):
+    course = gpx.tracks[0].segments[0]
+
+    start_lat = course.points[0].latitude
+    start_lon = course.points[0].longitude
+
+    fin_lat = course.points[-1].latitude
+    fin_lon = course.points[-1].longitude
+
+    # Start position marker
+    folium.Marker(
+        [start_lat, start_lon],
+        icon=folium.Icon(icon="play", prefix="fa", color="green"),
+    ).add_to(route_map)
+
+    # Finish position marker
+    folium.Marker(
+        [fin_lat, fin_lon],
+        icon=folium.Icon(icon="flag-checkered", prefix="fa", color="red"),
+    ).add_to(route_map)
+
+
+def display_gpx_on_map(route_map, gpx_input):
+    if not gpx_input:
+        return route_map
+
+    # Parse input .gpx file
+    gpx = gpxpy.parse(gpx_input)
+
+    # Display gpx track on map
+    coordinates = [
+        (point.latitude, point.longitude) for point in gpx.tracks[0].segments[0].points
+    ]
+    folium.PolyLine(coordinates, weight=6).add_to(route_map)
+
+    # Center the map on the track
+    route_map.fit_bounds(route_map.get_bounds(), padding=(30, 30))
+
+    _display_pois_on_map(route_map, gpx)
+
+    # Display start and finish location markers
+    _display_start_finish(route_map, gpx)
+
+    return route_map
+
+
+def _display_pois_on_map(route_map, gpx):
+    pois = _get_poi_info(gpx)
+
+    for poi in pois:
+        poi_lat, poi_lon = poi["feature_coords"]
+        folium.Marker(
+            [poi_lat, poi_lon],
+            icon=folium.Icon(icon="faucet-drip", prefix="fa", color="blue"),
+        ).add_to(route_map)
+
+
+instruction_md = "### Choose .GPX file"
+
+gpx_input = pn.widgets.FileInput(accept=".gpx")
+
+route_map = folium.Map(
+    location=[56.945695, 24.120704],
+    zoom_start=13,
+    tiles="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png",
+    attr='<a href="https://stadiamaps.com/">Stadia Maps</a',
+)
+
+
+button = pn.widgets.Button(name="Find water", button_type="primary")
+
+ctrl_row = pn.Row(instruction_md, gpx_input)  # , button)
+
+
+gspec = pn.GridSpec(sizing_mode="stretch_both", min_height=800)
+
+gspec[0:2, :30] = ctrl_row
+gspec[2:50, 0:100] = pn.bind(display_gpx_on_map, route_map, gpx_input)
+
+gspec.servable()
